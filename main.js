64f335391a71290fabef5fac87b317b398757b67
Pravin-Krishna-K <pravinkrishna.it2125@gmail.com>
2024-06-06 23:25:31 +0530

    rout structure

--- main.js
+++ main.js
@@ -10,6 +10,37 @@ app.get('/', (req,res)=>{
     res.json({msg: "Hello students!"})
 });
 
+
+
+// crud functionality of movies...
+
+
+// for reading
+app.get('/movies', ()=>{
+
+})
+
+
+// for creating 
+
+app.post('/movies', ()=>{
+
+})
+
+// for updating 
+
+app.put('/movies/:id', ()=>{
+
+})
+
+
+// for deleting
+
+app.delete('/movies/:id', ()=>{
+
+})
+
+
 app.listen(PORT, () =>{
     console.log(`The server is runnning at http://localhost:${PORT}`) // in programming when we are using same value in two different palce, we donot do 
                                                                        // because we follow DRY PRINCIPLE (DON'T REPEAT YOURSELF) 
